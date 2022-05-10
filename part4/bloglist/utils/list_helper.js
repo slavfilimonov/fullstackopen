e68f15171cdf5516922233c6172e2540e74d8c33
slavfilimonov <slavfilimonov@gmail.com>
2022-05-10 19:24:40 +0300

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,10 +3,28 @@ const dummy = (blogs) => {
 	return 1
 }
 
-const totalLikes = (blogs) => blogs
-	.reduce((sum, blog) => sum + blog.likes, 0)
+const totalLikes = (blogs) =>
+	blogs.reduce((sum, blog) =>
+		sum + blog.likes
+		, 0)
+
+const favoriteBlog = (blogs) =>
+	blogs.reduce((favorite, blog) =>
+		favorite.likes > blog.likes
+			? favorite
+			: {
+				title: blog.title,
+				author: blog.author,
+				likes: blog.likes
+			}
+		, {
+			title: "No blogs were found",
+			author: "No authors were found",
+			likes: 0
+		})
 
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 }
